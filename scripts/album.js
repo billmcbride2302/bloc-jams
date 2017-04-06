2a5de73817e612770300e07b1ac8aab0da203e4b
Bill McBride <bill@Bills-MacBook-Pro.local>
2017-04-06 10:56:15 -0400

    checkpoint 13

--- scripts/album.js
+++ scripts/album.js
@@ -29,10 +29,9 @@
      ]
  };
 
-var createSongRow = function(songNumber, songName, songLength) {
+ var createSongRow = function(songNumber, songName, songLength) {
      var template =
         '<tr class="album-view-song-item">'
-      + '  <td class="song-item-number">' + songNumber + '</td>'
       + '  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
       + '  <td class="song-item-title">' + songName + '</td>'
       + '  <td class="song-item-duration">' + songLength + '</td>'
@@ -41,45 +40,122 @@ var createSongRow = function(songNumber, songName, songLength) {
  
      return template;
  };
+
 var setCurrentAlbum = function(album) {
-     // #1
+    
+     // First, at #1, we select all of the HTML elements required to display on the album page: title, artist, release info, image, and song list. We want to populate these elements with information. To do so, we assign the corresponding values of the album objects' properties to the HTML elements.
+    
      var albumTitle = document.getElementsByClassName('album-view-title')[0];
      var albumArtist = document.getElementsByClassName('album-view-artist')[0];
      var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
      var albumImage = document.getElementsByClassName('album-cover-art')[0];
      var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
- 
-     // #2
+    
+     // At #2, the firstChild property identifies the first child node of an element, and  nodeValue returns or sets the value of a node. Alternatively, we could technically use  innerHTML to insert plain text (like we did in collection.js), but it's excessive and semantically misleading in this context because we aren't adding any HTML.
+    
      albumTitle.firstChild.nodeValue = album.title;
      albumArtist.firstChild.nodeValue = album.artist;
      albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
      albumImage.setAttribute('src', album.albumArtUrl);
  
-     // #3
+     // When we populated the Collection view with albums, we initially set the value of the parent container's innerHTML to an empty string. This ensured that we were working with a clean slate. We do the same here, at #3, and clear the album song list HTML to make sure there are no interfering elements.
+    
      albumSongList.innerHTML = '';
  
-     // #4
+     // We use a for loop, at #4, to go through all the songs from the specified album object and insert them into the HTML using the innerHTML property. The createSongRow function is called at each loop, passing in the song number, name, and length arguments from our album object.
+
      for (var i = 0; i < album.songs.length; i++) {
          albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
      }
  };
 
 var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
+var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
+var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
+
+ // Store state of playing songs
+var currentlyPlayingSong = null;
+
 var songRows = document.getElementsByClassName('album-view-song-item');
 
-var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
- 
- window.onload = function() {
-     setCurrentAlbum(albumPicasso);
 
- songListContainer.addEventListener('mouseover', function(event) {
+   var findParentByClassName = function(element, targetClass) {
+        if (element) {
+            var currentParent = element.parentElement;
+            while (currentParent.className !== targetClass && currentParent.className !== null) {
+                currentParent = currentParent.parentElement;
+            }
+            return currentParent;
+        }
+    };
+
+
+var getSongItem = function(element) {
+    switch (element.className) {
+        case 'album-song-button':
+        case 'ion-play':
+        case 'ion-pause':
+            return findParentByClassName(element, 'song-item-number');
+        case 'album-view-song-item':
+            return element.querySelector('.song-item-number');
+        case 'song-item-title':
+        case 'song-item-duration':
+            return findParentByClassName(element, 'album-view-song-item').querySelector('.song-item-number');
+        case 'song-item-number':
+            return element;
+        default:
+            return;
+    }  
+};
+
+var clickHandler = function(targetElement) {
+    
+         var songItem = getSongItem(targetElement);
+
+         if (currentlyPlayingSong === null) {
+             songItem.innerHTML = pauseButtonTemplate;
+             currentlyPlayingSong = songItem.getAttribute('data-song-number');
+         } else if (currentlyPlayingSong === songItem.getAttribute('data-song-number')) {
+             songItem.innerHTML = playButtonTemplate;
+             currentlyPlayingSong = null;
+         } else if (currentlyPlayingSong !== songItem.getAttribute('data-song-number')) {
+             var currentlyPlayingSongElement = document.querySelector('[data-song-number="' + currentlyPlayingSong + '"]');
+             currentlyPlayingSongElement.innerHTML = currentlyPlayingSongElement.getAttribute('data-song-number');
+             songItem.innerHTML = pauseButtonTemplate;
+             currentlyPlayingSong = songItem.getAttribute('data-song-number');
+         };
+    };
+
+//When the window finishes loading, make albumPicasso the current album. 
+window.onload = function() {
+    setCurrentAlbum(albumPicasso);
+    
+    songListContainer.addEventListener('mouseover', function(event) {
         if (event.target.parentElement.className === 'album-view-song-item') {
-        event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;         }    
-     });
- };
+                event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
+                var songItem = getSongItem(event.target);
 
-     for (var i = 0; i < songRows.length; i++) {
-         songRows[i].addEventListener('mouseleave', function(event) {
-             this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
-         });
-     }
+                if (songItem.getAttribute('data-song-number') !== currentlyPlayingSong) {
+                    songItem.innerHTML = playButtonTemplate;
+                }
+        }
+    });
+
+    for (var i = 0; i < songRows.length; i++) {
+             songRows[i].addEventListener('mouseleave', function(event) {
+                 // Selects first child element, which is the song-item-number element
+                 var songItem = getSongItem(event.target);
+                 var songItemNumber = songItem.getAttribute('data-song-number');
+
+                 // #2
+                 if (songItemNumber !== currentlyPlayingSong) {
+                     songItem.innerHTML = songItemNumber;
+                 };
+             });
+
+             songRows[i].addEventListener('click', function(event) {
+                 // Event handler call
+                 clickHandler(event.target);
+             });
+         };
+};
