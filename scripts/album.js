2f0b6598212839e5ab2e83b6869a32bca5372408
Bill McBride <bill@Bills-MacBook-Pro.local>
2017-04-10 17:15:18 -0400

    assignmnet 20 complete

--- scripts/album.js
+++ scripts/album.js
@@ -143,14 +143,34 @@ var setCurrentAlbum = function(album) {
 
  var $previousButton = $('.main-controls .previous');
  var $nextButton = $('.main-controls .next');
+ var $playPauseButton = $('.main-controls .play-pause');
+
+
+var togglePlayFromPlayerbar = function() {
+     var $currentlyPlayingCell = getSongNumberCell(currentlyPlayingSongNumber);
+    
+     if (currentSoundFile.isPaused()) {
+         $currentlyPlayingCell.html(pauseButtonTemplate);
+         $(this).html(playerBarPauseButton);
+         currentSoundFile.play();
+     } 
+    
+    else if (currentSoundFile) {
+         $currentlyPlayingCell.html(playButtonTemplate);
+         $(this).html(playerBarPlayButton);
+         currentSoundFile.pause();
+     }
+ };
+ 
 
 
 $(document).ready(function() {
     setCurrentAlbum(albumPicasso);
     
     $previousButton.click(previousSong);
-     $nextButton.click(nextSong);
+    $nextButton.click(nextSong);
     
+    $playPauseButton.click(togglePlayFromPlayerbar);
 });
 
 var trackIndex = function(album, song) {
